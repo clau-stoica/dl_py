174128b6c5ab74bd67572222b7c08663018ae8bb
clau-stoica <klau.stoica@gmail.com>
2017-06-02 21:00:23 +0300

    update pandas tut

--- start.py
+++ start.py
@@ -3,4 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 s = pd.Series([1,3,5,np.nan,6,8])
-print(s)
+# print(s)
+
+dates = pd.date_range('20170106', periods=6)
+# print(dates)
+
+df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=list('ABCD'))
+print df
